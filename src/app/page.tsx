65d066c44811f5208904a7ab76c102da34b30ff2
Ina <ina9377@gmail.com>
2026-04-18 14:09:05 +0900

    chore: clean up home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import KakaoMap from '@/components/map/KakaoMap';
-
 import { homePageDescription, homePageMain, homePageTitle } from './page.style';
+
+import KakaoMap from '@/components/map/KakaoMap';
 
 export default function Home() {
   return (
     <main className={homePageMain()}>
-      <h1 className={homePageTitle()}>우리동네 분리수거 ️</h1>
+      <h1 className={homePageTitle()}>우리동네 분리수거 </h1>
 
       <p className={homePageDescription()}>
         지도에서 지역을 선택하면 배출 기준을 볼 수 있어요!! (현재는 관악구,
